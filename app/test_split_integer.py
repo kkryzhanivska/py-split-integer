e97f6cbc7cf1121a3bbaebea5f616a471f0da23e
Karina Kryzh <krizanivskak@gmail.com>
2025-10-20 21:36:28 +0100

    fix

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,25 +2,31 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    goals = split_integer(32, 6)
-    assert sum(goals) == 32
+    assert sum(split_integer(17, 4)) == 17
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    goals = split_integer(10, 5)
-    assert goals == [2, 2, 2, 2, 2]
+    assert split_integer(6, 2) == [3, 3]
+    assert len(split_integer(3, 2)) == 2
+    assert all(isinstance(x, int) for x in split_integer(6, 2))
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    goals = split_integer(3, 1)
-    assert goals == [3]
+    assert split_integer(8, 1) == [8]
+    assert len(split_integer(8, 1)) == 1
+
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    goals = split_integer(13, 3)
-    assert goals == sorted(goals)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
+    assert split_integer(32, 6) == [5, 5, 5, 5, 6, 6]
+
+    assert split_integer(17, 4) == sorted(split_integer(17, 4))
+    assert max(split_integer(17, 4)) - min(split_integer(17, 4)) <= 1
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    goals = split_integer(1, 4)
-    assert goals == [0, 0, 0, 1]
+    assert sum(split_integer(3, 5)) == 3
+    assert len(split_integer(3, 5)) == 5
+    assert split_integer(3, 5) == sorted(split_integer(3, 5))
+    assert all(isinstance(x, int) for x in split_integer(3, 5))
